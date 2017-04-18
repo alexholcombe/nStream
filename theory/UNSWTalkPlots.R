21fef2bb7f85391825b2dd5ebb0cf83ccde47f7c
Alex O. Holcombe <aoholcombe@gmail.com>
2017-04-18 14:48:41 +1000

    graphics tweaks

--- theory/UNSWTalkPlots.R
+++ theory/UNSWTalkPlots.R
@@ -1,11 +1,11 @@
 rm(list=ls())
 library(ggplot2)
 source('../ggplotElements.R')
-theme_set(theme_apa(base_size = 20)) 
+theme_set(theme_apa(base_size = 20) ) 
 
 p <- .75
 nTrials <- 5000
-latency <- 1
+latency <- 0.6
 precision <- 1.5
 
 minError <- -17
@@ -14,43 +14,51 @@ maxError <- 17
 guessing <- runif((1-p)*nTrials, min = minError, max = maxError)
 normSelected <- rnorm(p*nTrials, mean = latency, sd = precision)
 
-logNormSelected <- rlnorm(p*nTrials, mean = 1.2, sd=log(1.5))
+logNorm_sd = 7
+logNorm_latency = 2
+logNormSelected <- rlnorm(p*nTrials, mean = logNorm_latency, sd=log(logNorm_sd))
 
 
-normObs <- data.frame(trial = c(rep('guessing', times = (1-p)*nTrials), rep('non-guessing', times = p*nTrials)), error = c(guessing, normSelected))
-logNormObs <- data.frame(trial = c(rep('guessing', times = (1-p)*nTrials), rep('non-guessing', times = p*nTrials)), error = c(guessing, logNormSelected))
+normObs <- data.frame(trial = c(rep('guessing', times = (1-p)*nTrials), 
+                      rep('non-guessing', times = p*nTrials)), error = c(guessing, normSelected))
+logNormObs <- data.frame(trial = c(rep('guessing', times = (1-p)*nTrials), 
+                         rep('non-guessing', times = p*nTrials)), error = c(guessing, logNormSelected))
 
 normErrorHist <- ggplot(normObs, aes(x=error))+
   geom_histogram(binwidth = 1)+
   labs(x='Serial Position Error', title = 'Errors')+
   theme(axis.text.x = element_text(family='Arial', size=20))+
-  lims(x=c(-17,17))
+  xlim(-17,17) + geom_vline(xintercept=0)
 
 normErrorHist
 
+ylims<-c(0,0.21)
 
 normErrorDensity <- ggplot(normObs, aes(x=error))+
   geom_density(adjust = 5,aes(fill = trial, colour = trial, alpha = trial))+
   scale_fill_manual(values = c('#feb24c', '#31a354'))+
   scale_colour_manual(values = c('#feb24c', '#31a354'))+
   scale_alpha_manual(values = c(1,.8))+
-  labs(x = 'Serial Position Error', fill = 'Trial', colour ='Trial', alpha = 'Trial', title = 'Underlying Model')+
+  labs(x = 'Serial Position Error', fill = 'Trial', colour ='Trial', alpha = 'Trial', title = 'Model')+
   theme(axis.text.x = element_text(family='Arial', size=20))+
-  lims(x=c(-17,17))
+  xlim(-17,17) + geom_vline(xintercept=0) + ylim(ylims)
 
 normErrorDensity
 
 logNormErrorHist <- ggplot(logNormObs, aes(x=error))+
   geom_histogram(binwidth = 1)+
-  labs(x='Serial Position Error', title = 'Empirical Errors')+
-
+  labs(x='Serial Position Error', title = 'Empirical Errors')
 
 logNormErrorDensity <- ggplot(logNormObs, aes(x=error))+
   geom_density(adjust = 5,aes(fill = trial, colour = trial, alpha = trial))+
   scale_fill_manual(values = c('#feb24c', '#31a354'))+
   scale_colour_manual(values = c('#feb24c', '#31a354'))+
   scale_alpha_manual(values = c(1,.8))+
-  labs(x = 'Serial Position Error', fill = 'Trial', colour ='Trial', alpha = 'Trial', title = 'Underlying Model')+
-  theme(axis.text.x = element_text(family='Arial', size=20))
+  xlim(-17,17) +  geom_vline(xintercept=0) 
+  labs(x = 'Serial Position Error', fill = 'Trial', colour ='Trial', alpha = 'Trial', title = 'Model') +
+  theme (plot.background = element_rect(fill = "transparent",colour = NA)) #so can save transparent image, hopefully
+#+  theme(axis.text.x = element_text(family='Arial', size=20)) 
 
-logNormErrorDensity
+logNormErrorDensity 
+
+logNormErrorDensity + ylim(ylims) 
