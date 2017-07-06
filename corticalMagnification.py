e2fbc995c17880c5f686bedd9de77953379acd67
Charlie Ludowici <charlie.ludowici@gmail.com>
2017-07-06 11:24:23 +1000

    scaled the magnification so that 3deg eccentric letters are 1deg high

--- corticalMagnification.py
+++ corticalMagnification.py
@@ -4,6 +4,18 @@ def corticalMagnification(stimulus, ltrHeight):
 	A = 29.2 #Cortical scaling factor
 	e2 = 3.67 #degrees
 
+	###The 2 vs 8 experiment had 1 degree letters at 3 degrees of eccentricity
+
+	###The scaling from the fovea to 3degrees is
+
+	foveaToThree = (3+e2)/e2
+
+	###Foveated letters would have to be this large to get a 1 degree high letter at 3 degrees of eccentricity
+
+	foveatedSize= 1/foveaToThree
+
+	ltrHeight = foveatedSize #this is the letter height at fixation (never really happens)
+
 	baseMagnification = A/(3+e2) #when E = 3
 
 	stimulusEccentricity = sqrt(stimulus.pos[0]**2 + stimulus.pos[1]**2) #E
