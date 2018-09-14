438453835608b9a30310b7b762bd6e0323cd1a18
Charlie Ludowici <charlie.ludowici@gmail.com>
2018-09-14 13:51:37 +1000

    Only fit new participants

--- Analysis/parameterAnalyses.R
+++ Analysis/parameterAnalyses.R
@@ -72,55 +72,8 @@ runAnyway <- FALSE #If TRUE, fit models regardless of the presence of a paramete
 
 nParamFiles <- length(list.files(path = 'modelOutput',pattern ='parameterEstimates.*\\.csv',full.names = T)) #How many parameter DFs are saved?
 
-if(nParamFiles==0 | runAnyway){
-  for(thisParticipant in unique(allErrors$ID)){
-    for(thisCondition in unique(allErrors$crowded)){
-      for(thisRing in unique(allErrors$ring)){
-        print(paste0('Participant: ', thisParticipant, '. Ring: ', thisRing, '. Condition: ', thisCondition))
-        theseParams <- allErrors %>% filter(., crowded == thisCondition, ring == thisRing, ID == thisParticipant) %>% analyzeOneCondition(., 24, bounds, nReps)
-        
-        if(theseParams$pLRtest<.05){
-          params %<>%
-            mutate(efficacy=replace(efficacy, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$efficacy)) %>%
-            as.data.frame()
-          
-          params %<>%
-            mutate(latency=replace(latency, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$latency)) %>%
-            as.data.frame()
-          
-          params %<>%
-            mutate(precision=replace(precision, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$precision)) %>%
-            as.data.frame()
-        } else {
-          params %<>%
-            mutate(efficacy=replace(efficacy, ID == thisParticipant & crowded == thisCondition & ring == thisRing, 0)) %>%
-            as.data.frame()
-          
-          params %<>%
-            mutate(latency=replace(latency, ID == thisParticipant & crowded == thisCondition & ring == thisRing, NaN)) %>%
-            as.data.frame()
-          
-          params %<>%
-            mutate(precision=replace(precision, ID == thisParticipant & crowded == thisCondition & ring == thisRing, NaN)) %>%
-            as.data.frame()
-        }
-        
-        params %<>%
-          mutate(val=replace(val, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$val)) %>%
-          as.data.frame()
-        
-        params %<>%
-          mutate(valGuessing=replace(valGuessing, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$valGuessing)) %>%
-          as.data.frame()
-        
-        params %<>%
-          mutate(pLRtest=replace(pLRtest, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$pLRtest)) %>%
-          as.data.frame()
-      }
-    }
-  }
-  write.csv(params, paste0('modelOutput/parameterEstimates',format(Sys.time(), "%d-%m-%Y_%H-%M-%S"),'.csv'))
-} else {
+if(nParamFiles>0){
+  print('we out here')
   paramFiles <- list.files(path = 'modelOutput',pattern ='parameterEstimates.*\\.csv',full.names = T) #What are the saved param files?
   
   splits <- strsplit(paramFiles, 'Estimates|(?<=[0-9][0-9])_|\\.csv',perl = T) #split out the date stamps from the param files names
@@ -129,9 +82,93 @@ if(nParamFiles==0 | runAnyway){
     unlist %>% 
     as.POSIXct(.,format='%d-%m-%Y %H-%M-%S') #Format them as POSIXct (datetime) 
   
-  whichMaxDate <- which(theseDates == max(theseDates)) #which is the oldest?
-  params <- read.csv(paramFiles[whichMaxDate]) #load the oldest
+  whichMaxDate <- which(theseDates == max(theseDates)) #which is the newest?
+  params <- read.csv(paramFiles[whichMaxDate]) #load the newest
+  
+  #Empty space in the param DF for the un-modelled participants
+  notModelled <- allErrors %>% filter(., !ID %in% params$ID) %>% pull(ID) %>% unique
+  
+  unModelledRows <- expand.grid(
+    ID = factor(notModelled),
+    crowded = factor(unique(allErrors$crowded)),
+    ring = factor(unique(allErrors$ring)),
+    efficacy = 999,
+    latency = 999,
+    precision = 999,
+    val = 999,
+    valGuessing = 999,
+    pLRtest = 999,
+    stringsAsFactors = F
+  )
+  
+  params <- rbind(params, unModelledRows)
+  
+} else {
+  notModelled <- allErrors %>% pull(ID) %>% unique
+  
+  params <- expand.grid(
+    ID = factor(notModelled),
+    crowded = factor(unique(allErrors$crowded)),
+    ring = factor(unique(allErrors$ring)),
+    efficacy = 999,
+    latency = 999,
+    precision = 999,
+    val = 999,
+    valGuessing = 999,
+    pLRtest = 999,
+    stringsAsFactors = F
+  )
+}  
+
+
+for(thisParticipant in notModelled){
+  for(thisCondition in unique(allErrors$crowded)){
+    for(thisRing in unique(allErrors$ring)){
+      print(paste0('Participant: ', thisParticipant, '. Ring: ', thisRing, '. Condition: ', thisCondition))
+      theseParams <- allErrors %>% filter(., crowded == thisCondition, ring == thisRing, ID == thisParticipant) %>% analyzeOneCondition(., 24, bounds, nReps)
+      
+      if(theseParams$pLRtest<.05){
+        params %<>%
+          mutate(efficacy=replace(efficacy, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$efficacy)) %>%
+          as.data.frame()
+        
+        params %<>%
+          mutate(latency=replace(latency, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$latency)) %>%
+          as.data.frame()
+        
+        params %<>%
+          mutate(precision=replace(precision, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$precision)) %>%
+          as.data.frame()
+      } else {
+        params %<>%
+          mutate(efficacy=replace(efficacy, ID == thisParticipant & crowded == thisCondition & ring == thisRing, 0)) %>%
+          as.data.frame()
+        
+        params %<>%
+          mutate(latency=replace(latency, ID == thisParticipant & crowded == thisCondition & ring == thisRing, NaN)) %>%
+          as.data.frame()
+        
+        params %<>%
+          mutate(precision=replace(precision, ID == thisParticipant & crowded == thisCondition & ring == thisRing, NaN)) %>%
+          as.data.frame()
+      }
+      
+      params %<>%
+        mutate(val=replace(val, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$val)) %>%
+        as.data.frame()
+      
+      params %<>%
+        mutate(valGuessing=replace(valGuessing, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$valGuessing)) %>%
+        as.data.frame()
+      
+      params %<>%
+        mutate(pLRtest=replace(pLRtest, ID == thisParticipant & crowded == thisCondition & ring == thisRing, theseParams$pLRtest)) %>%
+        as.data.frame()
+    }
+  }
 }
+write.csv(params, paste0('modelOutput/parameterEstimates',format(Sys.time(), "%d-%m-%Y_%H-%M-%S"),'.csv'),row.names = F)
+
 
 params$ring %<>% as.factor
 
@@ -216,9 +253,9 @@ ggplot(paramsForAnalysis, aes(x=crowded, y = precision))+
 ########################
 
 if(participantPlots){
-  for(thisParticipant in unique(allErrors$ID)){
-    for(thisCondition in unique(allErrors$crowded)){
-      for(thisRing in unique(allErrors$ring)){
+  for(thisParticipant in unique(paramsForAnalysis$ID)){
+    for(thisCondition in unique(paramsForAnalysis$crowded)){
+      for(thisRing in unique(paramsForAnalysis$ring)){
         thisEfficacy <- params %>% filter(ID == thisParticipant & crowded == thisCondition & ring == thisRing) %>% pull(efficacy)
         thisLatency <- params %>% filter(ID == thisParticipant & crowded == thisCondition & ring == thisRing) %>% pull(latency)
         thisPrecision <- thisEfficacy <- params %>% filter(ID == thisParticipant & crowded == thisCondition & ring == thisRing) %>% pull(precision)
