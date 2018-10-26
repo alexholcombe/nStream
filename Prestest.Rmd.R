ca3e4c4db4c4f30edee18b376dccc3f1d1e3634b
Charlie Ludowici <charlie.ludowici@gmail.com>
2018-10-26 14:57:22 +1100

    check prop before cue from model recovery

---
+++ Prestest.Rmd.R
@@ -0,0 +1,10 @@
+---
+  title: "Introduction to R Markdown"
+output:
+  revealjs::revealjs_presentation:
+  css: presentation.css
+reveal_options:
+  margin: 0.1
+self_contained: false
+reveal_plugins: ["notes", "search"]
+---
