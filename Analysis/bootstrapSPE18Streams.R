31b0bc92a4e6c9fd0789d7922d7a741618db8c03
Charlie Ludowici <charlie.ludowici@gmail.com>
2019-04-06 17:30:20 -0400

    add example plots for figures

--- Analysis/bootstrapSPE18Streams.R
+++ Analysis/bootstrapSPE18Streams.R
@@ -8,6 +8,7 @@ setwd('~/gitCode/nStream/')
 allErrors <- read.csv('Analysis/allErrors18Streams.txt', header = T, stringsAsFactors = F)
 
 runAnyway <- FALSE
+plotExample <- FALSE #Generates the components of the bootstrap explainer plot
 xDomain <- -4:4
 
 bootstrapPValue <- function(theseData, numItemsInStream, whichSPE, nReps){
@@ -116,18 +117,134 @@ table %>% dcast(.,
 
 
 bootstrapPlot <- table %>% ggplot(., aes(x=xDomain, y = nSig))+
-  geom_line(aes(linetype = factor(condition)),size = 1)+
+  geom_line(aes(linetype = factor(condition)))+
   scale_x_continuous(breaks = -9:9)+
   scale_y_continuous(breaks = seq(0,12,3))+
-  labs(x = 'SPE', y = 'Deviations from Guessing',linetype = 'nStreams')
+  labs(x = 'SPE', y = 'Deviations from Guessing',linetype = 'nStreams')+
+  scale_linetype_manual(values = c('2' = 'solid', '6' = 'dotted', '18' = 'twodash'))
 
 bootstrapPlot
 
-numNeg
-
 ggsave(filename = 'modelOutput/18Streams/bootstrapPlot.png',
        plot = bootstrapPlot,
-       width=29.21, 
-       height=12.09,
+       width=30, 
+       height=30,
        units='cm')
 
+#######################
+###Example Bootstrap###
+#######################
+
+if(plotExample){
+  randID <- sample(allErrors$ID, size = 1)
+  exampleCondition = 2
+  whichSPE = 2
+  nReps <- 5000
+  
+  exampleData <- allErrors %>% filter(ID == randID, condition == exampleCondition, !fixationReject)
+  exampleNTrials <- nrow(exampleData)
+  
+  exampleHistogram <- ggplot(exampleData, aes(x = SPE))+
+    geom_histogram(binwidth = 1, aes(fill = SPE == whichSPE))+
+    geom_vline(xintercept = 0, linetype = 'dashed')+
+    scale_x_continuous(breaks = seq(min(exampleData$SPE),max(exampleData$SPE),2))+
+    scale_fill_manual(values = c('TRUE' = '#ffa951', 'FALSE' = '#628093'), guide = FALSE)+
+    labs(y = 'Count')+
+    theme_apa()
+  
+  exampleHistogram
+  
+  ggsave(exampleHistogram, 
+         file = paste0('modelOutput/18Streams/bootstrapExampleEmpirical', randID,'.png'),
+         width = 10,
+         height = 10,
+         units = 'cm')
+  
+  exampleAccuracy <- length(which(exampleData$SPE == 0))/exampleNTrials
+  
+  exampleGuessingRate <- 1 - exampleAccuracy
+  print(exampleAccuracy)
+  print(exampleGuessingRate)
+  
+  maxSPETheseData <- exampleData %>% pull(targetSP) %>% max
+  minSPETheseData <- exampleData %>% pull(targetSP) %>% min
+  minSPE <- 1 - maxSPETheseData
+  maxSPE <- numItemsInStream - minSPETheseData
+  
+  thisGuessingDist <- createGuessingDistribution(minSPE = minSPE,
+                                                 maxSPE = maxSPE,
+                                                 targetSP = exampleData$targetSP,
+                                                 numItemsInStream = numItemsInStream)
+  
+  pseudoUniform <- data.frame(xDomain = minSPE:maxSPE, guessingDist = thisGuessingDist, prob = (thisGuessingDist/sum(thisGuessingDist))) #give guessing dist values labels
+  
+  
+  exampleGuessingDist <- ggplot(pseudoUniform, aes(x = xDomain, y = prob))+
+    geom_bar(stat= 'identity', aes(fill = xDomain == whichSPE))+
+    labs(x = 'SPE', y = 'Probability of Response')+
+    scale_fill_manual(values = c('TRUE' = '#ffa951', 'FALSE' = '#628093'), guide = FALSE)+
+    theme_apa()
+  exampleGuessingDist
+  
+  ggsave(exampleGuessingDist, 
+         file = paste0('modelOutput/18Streams/bootstrapExampleGuessing', randID,'.png'),
+         width = 10,
+         height = 10,
+         units = 'cm')
+  
+  nWhichSPE <- exampleData %>% filter(SPE == whichSPE) %>% nrow() #How many observations at SPE = whichSPE?
+  
+  bootstraps <- data.frame(rep = 1:nReps, #store the counts of whichSPE sampled from the pseudouniform here.
+                           count = -999)
+  
+  
+  theseSamples <- expand.grid(sample = 1:nReps, SPE = numeric(exampleNTrials*exampleGuessingRate))
+  
+  for(i in 1:nReps){
+    thisSample <- sample(pseudoUniform$xDomain, prob = pseudoUniform$prob, replace = T, size = exampleNTrials*exampleGuessingRate) #sample nTrials many trials from the pseudouniform over the range of possible SPEs in this experiment
+    theseSamples %<>% mutate(SPE = replace(SPE, sample == i, thisSample))
+    nThisSPE<- which(thisSample == whichSPE) %>% length #How many of the trials had an SPE == whichSPE
+    bootstraps %<>% mutate(count = replace(count, rep == i, nThisSPE)) #Put the count nThisSPE in the dataframe
+  }
+  
+  randomSamplesForPlotting <- sample(1:nReps, 12, replace = F)
+  
+  randomSamples <- theseSamples %>% filter(sample %in% randomSamplesForPlotting) %>% 
+    ggplot(., aes(x = SPE))+
+    geom_histogram(binwidth = 1, aes(fill = SPE == whichSPE))+
+    scale_fill_manual(values = c('TRUE' = '#ffa960', 'FALSE' = '#628093'), guide = FALSE)+
+    labs(y = 'Count')+
+    theme_apa()+
+    theme(
+      strip.background = element_blank(),
+      strip.text.x = element_blank()
+    )+
+    facet_wrap(~sample, nrow = 4)
+  
+  randomSamples
+  
+  ggsave(randomSamples, 
+         file = paste0('modelOutput/18Streams/randomBootstrapHistograms', randID,'.png'),
+         width = 10,
+         height = 10,
+         units = 'cm')
+  
+  bootstrapCountDist <- ggplot(bootstraps, aes(x = count))+
+    geom_histogram(binwidth = 1, aes(fill = count >= nWhichSPE))+
+    scale_fill_manual(values = c('TRUE' = '#ffa951', 'FALSE' = '#628093'), guide = FALSE)+
+    labs(x = 'Number of SPEs = 2', y = 'Number of Samples')+
+    theme_apa()
+  
+  ggsave(bootstrapCountDist, 
+         file = paste0('modelOutput/18Streams/bootstrapCountDistribution', randID,'.png'),
+         width = 10,
+         height = 10,
+         units = 'cm')
+  
+  
+  
+  
+  thisP <- length(which(bootstraps$count>=nWhichSPE))/nReps #a p.value. The proportion of bootstrapped samples that had a count of whichSPE at least as great as the observed count
+  
+  
+}
