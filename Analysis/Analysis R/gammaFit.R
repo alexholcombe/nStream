e1b3b47fbe6b3e1528435eaa18b1592f83133396
Charlie Ludowici <charlie.ludowici@gmail.com>
2019-04-30 10:45:43 -0400

    BF for guessing. Shape and scale

--- Analysis/Analysis R/gammaFit.R
+++ Analysis/Analysis R/gammaFit.R
@@ -13,6 +13,21 @@ devtools::load_all('~/gitCode/mixRSVP/')
 
 rm(list=ls())
 
+guessingDistributionBIC <- function(df){
+  columnsPresent <- all(c('val','valGuessing') %in% colnames(df))
+  if(!columnsPresent){
+    missingCols <- c('val','valGuessing')[!c('val','valGuessing') %in% colnames(df)]
+    stop('Missing ', missingCols, ' from df')
+  }
+  
+  BICVal <- log(100)*3+(2*df$val)
+  BICValGuessing <- log(100)*0+(2*df$valGuessing)
+  
+  deltaBIC <- BICValGuessing - BICVal
+  
+  data.frame(BFGuessVsMix = exp(deltaBIC/2))
+}
+
 timeStamp <- Sys.time() %>% strftime(format = '%d-%m-%Y_%H-%M')
 
 setwd('~/gitCode/nStream/')
@@ -79,127 +94,165 @@ lastCondition <- 'None'
 
 startRow <- 1
 
-for(thisFile in matlabDataFiles){
-  thisMatlab <- readMat(thisFile)
-  thisID <- thisMatlab$participantID[[1]]
-  thisCondition <- thisMatlab$itemRate[[1]]
-  if(thisID==lastID & thisCondition == lastCondition){
-    trialN <- (lastTrialN+1):(lastTrialN+25)
-  }else{
-    trialN = 1:25
-  }
-  for(thisStream in 1:2){
-    endRow <- startRow + 24
-    theseStimuli <- apply(thisMatlab$allLetterOrder[,thisStream,],2,function(i) stimuli[i]) %>% 
-      apply(., 1, paste0, collapse = '')
-    
-    
-    theseTargets <- thisMatlab$allTargets[,thisStream]
-    
-    theseResponseSPs <- c()
-    theseResponses <- c()
-    
-    for(thisTrial in 1:25){
-      actualTrialN <- trialN[thisTrial]
-      thisResponse <- thisMatlab$allResponses[thisTrial, thisStream]
-      thisLetterOrder <- thisMatlab$allLetterOrder[thisTrial, thisStream,]
-      thisSP <- which(thisLetterOrder == thisResponse)
-      theseResponseSPs <- c(theseResponseSPs, thisSP)
-      theseResponses <- c(theseResponses, stimuli[thisResponse])
+dataFiles <- list.files(
+  path = 'Analysis',
+  pattern = 'allData',
+  full.names = T
+  )
+
+if(length(dataFiles)>1){
+  allData <- read.csv(dataFiles[1], stringsAsFactors = F)
+} else {
+  for(thisFile in matlabDataFiles){
+    thisMatlab <- readMat(thisFile)
+    thisID <- thisMatlab$participantID[[1]]
+    thisCondition <- thisMatlab$itemRate[[1]]
+    if(thisID==lastID & thisCondition == lastCondition){
+      trialN <- (lastTrialN+1):(lastTrialN+25)
+    }else{
+      trialN = 1:25
     }
-    
-    theseSPEs <- theseResponseSPs - theseTargets
-    
-    
-    temp <- data.frame(
-      ID = thisID,
-      trial = trialN,
-      condition = thisCondition,
-      stream = thisStream,
-      SPE = theseSPEs,
-      targetSP = theseTargets,
-      response = theseResponses,
-      stimuli = theseStimuli,
-      stringsAsFactors = F
-    )
-    
-    allData[startRow:endRow,] <- temp
-    
-    startRow <- endRow + 1
+    for(thisStream in 1:2){
+      endRow <- startRow + 24
+      theseStimuli <- apply(thisMatlab$allLetterOrder[,thisStream,],2,function(i) stimuli[i]) %>% 
+        apply(., 1, paste0, collapse = '')
+      
+      
+      theseTargets <- thisMatlab$allTargets[,thisStream]
+      
+      theseResponseSPs <- c()
+      theseResponses <- c()
+      
+      for(thisTrial in 1:25){
+        actualTrialN <- trialN[thisTrial]
+        thisResponse <- thisMatlab$allResponses[thisTrial, thisStream]
+        thisLetterOrder <- thisMatlab$allLetterOrder[thisTrial, thisStream,]
+        thisSP <- which(thisLetterOrder == thisResponse)
+        theseResponseSPs <- c(theseResponseSPs, thisSP)
+        theseResponses <- c(theseResponses, stimuli[thisResponse])
+      }
+      
+      theseSPEs <- theseResponseSPs - theseTargets
+      
+      
+      temp <- data.frame(
+        ID = thisID,
+        trial = trialN,
+        condition = thisCondition,
+        stream = thisStream,
+        SPE = theseSPEs,
+        targetSP = theseTargets,
+        response = theseResponses,
+        stimuli = theseStimuli,
+        stringsAsFactors = F
+      )
+      
+      allData[startRow:endRow,] <- temp
+      
+      startRow <- endRow + 1
+    }
+    lastTrialN <- max(trialN)
+    lastID <- thisID
+    lastCondition <- thisCondition
   }
-  lastTrialN <- max(trialN)
-  lastID <- thisID
-  lastCondition <- thisCondition
+  allDataFile <- paste0('Analysis/allData_', timeStamp, '.csv')
+  
+  write.csv(file = allDataFile,
+            x = allData, 
+            row.names = F)
 }
 
 
-allData %>% filter(condition == 6) %>%
-  ggplot(aes(x = SPE))+
-  geom_histogram(binwidth = 1)+
-  facet_wrap(~ID + stream)
+paramFiles <- list.files(path = 'Analysis/Gamma Fits',
+                         pattern = 'params.*csv',
+                         full.name = T)
 
-for(thisParticipant in IDs){
-  for(thisCondition in conditions){
+if(length(paramFiles)>0){
+  paramsDF <- read.csv(paramFiles[1], stringsAsFactors = F)
+} else{
+  for(thisParticipant in IDs){
+    for(thisCondition in conditions){
       for(thisStream in 1:2){
-      theseData <- allData %>% filter(ID == thisParticipant, condition == thisCondition, stream == thisStream)
-      
-      cat('Participant: ', thisParticipant, '. Condition: ', thisCondition, '. Stream: ', thisStream ,'. Fitting gamma model                                  \r', sep = '')
-      paramsGamma <- theseData %>% analyzeOneConditionDF(numLettersInStream, parameterBoundsGamma, nReplicates = nReplications, modelKind = 'Gamma')
-      cat('Participant: ', thisParticipant, '. Condition: ', thisCondition, '. Stream: ', thisStream , '. Fitting normal model                                  \r', sep = '')
-      paramsN <- theseData %>% analyzeOneConditionDF(numLettersInStream, parameterBoundsNormal, nReplicates = nReplications, modelKind = 'Normal')
-      
-      paramsDF %<>% mutate(
-        efficacy = replace(efficacy, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$efficacy),
-        latency = replace(latency, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$latency),
-        precision = replace(precision, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$precision),
-        val = replace(val, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$val),
-        valGuessing = replace(valGuessing, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$valGuessing),
-        pLRtest = replace(pLRtest, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$pLRtest)
-      )
-      
-      paramsDF %<>% mutate(
-        efficacy = replace(efficacy, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$efficacy),
-        latency = replace(latency, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$latency),
-        precision = replace(precision, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$precision),
-        val = replace(val, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$val),
-        valGuessing = replace(valGuessing, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$valGuessing),
-        pLRtest = replace(pLRtest, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$pLRtest)
-      )
+        theseData <- allData %>% filter(ID == thisParticipant, condition == thisCondition, stream == thisStream)
+        
+        cat('Participant: ', thisParticipant, '. Condition: ', thisCondition, '. Stream: ', thisStream ,'. Fitting gamma model                                  \r', sep = '')
+        paramsGamma <- theseData %>% analyzeOneConditionDF(numLettersInStream, parameterBoundsGamma, nReplicates = nReplications, modelKind = 'Gamma')
+        cat('Participant: ', thisParticipant, '. Condition: ', thisCondition, '. Stream: ', thisStream , '. Fitting normal model                                  \r', sep = '')
+        paramsN <- theseData %>% analyzeOneConditionDF(numLettersInStream, parameterBoundsNormal, nReplicates = nReplications, modelKind = 'Normal')
+        
+        paramsDF %<>% mutate(
+          efficacy = replace(efficacy, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$efficacy),
+          latency = replace(latency, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$latency),
+          precision = replace(precision, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$precision),
+          val = replace(val, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$val),
+          valGuessing = replace(valGuessing, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$valGuessing),
+          pLRtest = replace(pLRtest, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Normal', paramsN$pLRtest)
+        )
+        
+        paramsDF %<>% mutate(
+          efficacy = replace(efficacy, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$efficacy),
+          latency = replace(latency, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$latency),
+          precision = replace(precision, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$precision),
+          val = replace(val, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$val),
+          valGuessing = replace(valGuessing, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$valGuessing),
+          pLRtest = replace(pLRtest, participant == thisParticipant & condition == thisCondition & stream == thisStream & model == 'Gamma', paramsGamma$pLRtest)
+        )
+      }
     }
   }
+  BICs <- paramsDF %>%
+    compareMixtureModels(params = .,
+                         nParam = nParams,
+                         nObs = 100,
+                         variables = c('condition', 'participant', 'stream'))
+  
+  
+  paramsDF <- BICs %>% mutate(favouredModel = 'Neither') %>%
+    mutate(favouredModel = replace(favouredModel, BF >3, 'Normal')) %>%
+    mutate(favouredModel = replace(favouredModel, BF<3^-1, 'Gamma')) %>%
+    select(participant, condition, stream, BF, favouredModel) %>%
+    left_join(paramsDF, ., by = c('participant', 'condition', 'stream')) #Add all this information to the paramDF 
+  
+  paramsFile <- paste0('Analysis/Gamma Fits/paramsDF_', timeStamp, '.csv')
+  
+  write.csv(file = paramsFile,
+            x = paramsDF, 
+            row.names = F)
 }
 
+#########################################################
+###Compute BFs comparing the mixtture to guessing only###
+#########################################################
 
-BICs <- paramsDF %>%
-  compareMixtureModels(params = .,
-                       nParam = nParams,
-                       nObs = 100,
-                       variables = c('condition', 'participant', 'stream'))
+paramsDF %<>% 
+  group_by(participant, condition, stream, model) %>% 
+  do(guessingDistributionBIC(.)) %>% 
+  mutate(
+    guessOrMixture = ifelse(BFGuessVsMix < 1/3, 'Guessing Only', 
+                            ifelse(BFGuessVsMix > 3, 'Mixture', 'Neither')
+    )
+  ) %>% 
+  left_join(paramsDF, ., by = c('participant', 'condition', 'stream','model'))
 
+paramsDF %<>%  mutate(
+  latency = replace(latency, guessOrMixture == 'Guessing Only', NA),
+  precision = replace(precision, guessOrMixture == 'Guessing Only', NA),
+  efficacy = replace(efficacy, guessOrMixture == 'Guessing Only', 0)
+)
 
-paramsDF <- BICs %>% mutate(favouredModel = 'Neither') %>%
-  mutate(favouredModel = replace(favouredModel, BF >3, 'Normal')) %>%
-  mutate(favouredModel = replace(favouredModel, BF<3^-1, 'Gamma')) %>%
-  select(participant, condition, stream, BF, favouredModel) %>%
-  left_join(paramsDF, ., by = c('participant', 'condition', 'stream')) #Add all this information to the paramDF 
-
-paramsFile <- paste0('Analysis/Gamma Fits/paramsDF_', timeStamp, '.csv')
-
-write.csv(file = paramsFile,
-          x = paramsDF, 
-          row.names = F)
-
-allDataFile <- paste0('Analysis/allData_', timeStamp, '.csv')
-
-write.csv(file = allDataFile,
-          x = allData, 
-          row.names = F)
+#################################################################
+###Create shape and scale variables. Compute gamma mean and sd###
+#################################################################
 
 paramsDF %<>% mutate(
-  efficacy = replace(efficacy, pLRtest >=.05, 0),
-  latency = replace(latency, pLRtest >= .05, NA),
-  precision = replace(precision, pLRtest >= .05, NA)
+  shape = ifelse(model == 'Gamma', latency, NA),
+  scale = ifelse(model == 'Gamma', precision, NA),
+) %>% mutate(
+  latency = ifelse(model == 'Gamma', shape*scale, latency),
+  precision = ifelse(model == 'Gamma', sqrt(shape*(scale^2)), precision)
 )
+
+
 
 
 #############
@@ -208,16 +261,14 @@ paramsDF %<>% mutate(
 
 
 densities <- paramsDF %>% #Purrr is new to me
-  select(participant, condition, stream, efficacy, latency, precision, model, favouredModel)%>% #Select the columns with the variables we want
-  pmap_dfr(function(efficacy, latency, condition, stream, precision, participant, model, favouredModel){ #For each row, compute the density over a range of milliseconds and return a dataframe
-    SPE <- seq(-500,1000,1)/(1000/12)
+  select(participant, condition, stream, efficacy, latency, precision, shape, scale, model, favouredModel)%>% #Select the columns with the variables we want
+  pmap_dfr(function(participant, condition, stream, efficacy, latency, shape, scale, precision, model, favouredModel){ #For each row, compute the density over a range of milliseconds and return a dataframe
+    SPE <- seq(-5,10,.1)
     print(paste0('favouredModel: ', favouredModel, '. model: ', model, '. participant: ', participant))
     if(efficacy > 0){
       if(favouredModel == 'Gamma'){
         if(model == 'Gamma'){
-          shape = latency
-          rate = precision
-          density =dgamma(SPE, shape = shape, scale = rate)
+          density =dgamma(SPE, shape = shape, scale = scale)
           data.frame(ID = participant,
                      condition = condition,
                      stream = stream,
@@ -241,9 +292,7 @@ densities <- paramsDF %>% #Purrr is new to me
         }
       } else {
         if(model == 'Gamma'){
-          shape = latency
-          rate = precision
-          density =dgamma(SPE, shape = shape, scale = rate)
+          density =dgamma(SPE, shape = shape, scale = scale)
           data.frame(ID = participant,
                      condition = condition,
                      stream = stream,
@@ -263,10 +312,11 @@ densities <- paramsDF %>% #Purrr is new to me
                      favouredModel = favouredModel,
                      stringsAsFactors = F)
         }
-    }
+      }
     }
   }
-  )
+)
+
 
 
 for(thisID in IDs){
@@ -278,13 +328,24 @@ for(thisID in IDs){
     
     thisPlot <- ggplot(theseObservations, aes(x = SPE))+
       geom_histogram(binwidth = 1)+
-      geom_line(data = theseDensities, aes(x = SPE, y = density*50, colour = model)) +
-      labs(title = paste0('Participant: ', thisIDNoSpace,'. Condition: ', thisCondition))+
+      geom_line(data = theseDensities, aes(x = SPE, y = density*100, colour = model)) +
+      labs(title = paste0('Participant: ', thisIDNoSpace))+
       geom_vline(xintercept = 0, linetype = 'dashed') +
       facet_grid(rows = vars(condition), cols = vars(stream), labeller = 'label_both')
-        
-      ggsave(filename = paste0('Analysis/Gamma Fits/GammaPlots/',thisIDNoSpace,'.png'),
-             plot = thisPlot, width = 29.21, height = 12.09, units = 'cm'
-      )
+    
+    ggsave(filename = paste0('Analysis/Gamma Fits/GammaPlots/',thisIDNoSpace,'.png'),
+           plot = thisPlot, width = 29.21, height = 12.09, units = 'cm'
+    )
 } 
+
+#################################
+###Latencies relative to onset###
+#################################
+
+paramsDF %<>% mutate(
+  latency = latency * (1000/condition),
+  latencyRelativeOnset = latency + (1000/condition)*.6
+) %>% mutate(
+  latencyRelativeOnset = replace(latencyRelativeOnset, model == 'Gamma', NA)
+  )
 
