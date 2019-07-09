bc9734247df61792f1a94aef89bc00cd6b704e83
Charlie Ludowici <charlie.ludowici@gmail.com>
2019-07-09 15:30:11 +1000

    save bootstrap results

--- Analysis/Reconsidering Temporal Selection Datasets/bootstrap.R
+++ Analysis/Reconsidering Temporal Selection Datasets/bootstrap.R
@@ -5,6 +5,8 @@ library(magrittr)
 library(dplyr)
 devtools::load_all('../mixRSVP/')
 
+rm(list=ls())
+
 ABData <- read.csv('Analysis/Reconsidering Temporal Selection Datasets/reconsideringTheABData.csv', stringsAsFactors = F)
 ABData %<>% filter(dataSet == 'MIT')
 
@@ -18,23 +20,27 @@ pvalues <- data.frame(
   participant = numeric(1e4),
   SPE = numeric(1e4),
   lag = numeric(1e4),
+  count = numeric(1e4),
   pvalue = numeric(1e4),
   stringsAsFactors = F
 )
 
 thisRow <- 1
 for(thisDataSet in dataSets){
-  theseParticipants <- ABData %>% filter(dataSet == thisDataSet) %>% pull(participant) %>% unique
+  theseParticipants <- ABData %>% filter(dataSet == thisDataSet) %>% pull(participant) %>% unique %>% sort
   for(thisParticipant in theseParticipants){
-    theseLags <- ABData %>% filter(dataSet == thisDataSet, participant == thisParticipant) %>% pull(lag) %>% unique
+    theseLags <- ABData %>% filter(dataSet == thisDataSet, participant == thisParticipant) %>% pull(lag) %>% unique %>% sort
     for(thisLag in theseLags){
       cat('dataSet:', thisDataSet, ', participant:', thisParticipant, 'lag:', thisLag,'                                        \r')
       theseData <- ABData %>% filter(dataSet == thisDataSet, participant == thisParticipant, lag == thisLag)
       thisPvalue <- bootstrapPValue(theseData, 26, -1)
-      pvalues[thisRow,] <- data.frame(thisDataSet, thisParticipant, -1, thisLag, thisPvalue)
+      thisCount <- length(which(theseData$SPE == 0))
+      pvalues[thisRow,] <- data.frame(thisDataSet, thisParticipant, -1, thisLag, thisCount, thisPvalue)
       thisRow <- thisRow+1
     }
   }
 }
 
 pvalues <- pvalues[1:(thisRow-1),]
+
+write.csv(x = pvalues, file = 'Analysis/Reconsidering Temporal Selection Datasets/pvalues.csv', row.names = F)
